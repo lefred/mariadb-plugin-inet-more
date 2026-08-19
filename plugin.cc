a8148cce9a7ca0bcf5fc0e7a0b171cba31e8e66e
Frederic Descamps <lefred.descamps@gmail.com>
2026-08-19 15:09:04 +0200

    sync plugin and release version
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- plugin.cc
+++ plugin.cc
@@ -85,5 +85,5 @@ maria_declare_plugin(inet_more){MariaDB_FUNCTION_PLUGIN,
      0x0100,
      NULL,
      NULL,
-     "1.0",
+     "0.2.0",
      MariaDB_PLUGIN_MATURITY_BETA} maria_declare_plugin_end;
